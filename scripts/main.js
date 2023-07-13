bec7a854e90e332df30496549ef0610d2908eb1f
DonoCodePeriod <135299863+DonoCodePeriod@users.noreply.github.com>
2023-07-12 23:52:03 -0400

    more troubleshooting on manifest.json and main.js' '

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+console.log("Hello")
+
+// have a document
+  // have a action, when the persons clic on pop up 
+
+// declare a body to select the body 
+
+// event 
+//invoke a function after google extention is click 
+  // button attached to top right of document. on 'clock'
+
+  // cat image 
+    // check, if the 
+// const test = document.querySelectorAll("div");
+// for(let i = 0;i<test.length;i++){
+//   test[i].style.color = red;
+//   test[i].style.backgroundColor = red;
+// }
+
+// test.style.color = green;
+setTimeout(()=>{
+
+const body = document.querySelector('html');
+console.log(body.style);
+//body.style.backgroundColor = "red";
+},3000)
+
+// console.log(bodyPage);
+
+  console.log('hello WORld Rick Ross')
+  
