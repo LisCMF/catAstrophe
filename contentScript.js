de31b8812bd7bc4ec0fce98da2618d675308a4e7
Lis Medina Frey <lismedina@protonmail.com>
2023-07-13 15:38:39 -0400

    " Jul 13 23"

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,83 @@
 
-// Listen for messages from the background script
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'replaceImages') {
-      // Select all the images on the page
-      const images = document.getElementsByTagName('img');
+// a div for the area of the screen that we are using 
+  const divMain = document.createElement('div');
+
+   // center to position the div
+   divMain.style.position = 'fixed';
+   divMain.style.left = '0';
+   divMain.style.top = '0';
+
+   // bring the div to the front
+   divMain.style.zIndex = '9999';
+
+  // we want the div to cover the page (make transparent later)
+   divMain.style.width = '50%';
+   divMain.style.height = '99%';
+   divMain.style.height = '99%';
+
+   // make it transparent green or grey
+   divMain.style.backgroundColor = 'rgba(0, 100, 0, 0.5)';
+
+
+// add it to the body end
+    document.body.appendChild(divMain);
+
+  // add a class to the div so we can make other manual changes
+ 
+  const dancingCat1 = document.createElement('img');
+
+  dancingCat1.setAttribute('src', 'https://media0.giphy.com/media/kVlLRtRWAVsDC/200w.gif?cid=790b7611bag056xmerusp5cxzxtemxv0y0sjwvnfuywdr7pq&ep=v1_gifs_search&rid=200w.gif&ct=g');
+
+  // size of the image
+  // dancingCat1.style.position = 'fixed';
+  // dancingCat1.style.left = '0';
+  // dancingCat1.style.top = '0';
+  // dancingCat1.style.zIndex = '9999';
+  // dancingCat1.setAttribute('zIndex', '9999');
+
+  // dancingCat1.style.width = '50%';
+  // dancingCat1.style.height = '99%';
+
+   // add divMain to the body end
+   divMain.appendChild(dancingCat1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     if (request.action === 'replaceImages') {
+//       // Select all the images on the page
+//       const images = document.getElementsByTagName('img');
   
-      // Loop through the images and replace the src with a random dancing cats GIF
-      for (let i = 0; i < images.length; i++) {
-        const randomCatIndex = Math.floor(Math.random() * 10) + 1;
-        const dancingCatsGIF = `dancing-cats${randomCatIndex}.gif`;
-        images[i].src = chrome.extension.getURL(dancingCatsGIF);
-      }
-    }
-  });
+//       // Loop through the images and replace the src with a random dancing cats GIF
+//       for (let i = 0; i < images.length; i++) {
+//         const randomCatIndex = Math.floor(Math.random() * 10) + 1;
+//         const dancingCatsGIF = `dancing-cats${randomCatIndex}.gif`;
+//         images[i].src = chrome.extension.getURL(dancingCatsGIF);
+//       }
+//     }
+//   });
+
+
+
 
 /*
 //-------------
@@ -183,4 +248,29 @@ divMain.classList.add('ourMainDiv');
 // // console.log(bodyPage);
 
 //   console.log('hello WORld Rick Ross')
+
+
+// // Listen for messages from the background script
+
+
+// // setTimeout(()=>{
+// //     const images = document.getElementsByTagName('img');
+  
+// //     // Loop through the images and replace the src with a random dancing cats GIF
+// //     for (let i = 0; i < images.length; i++) {
+// //       const randomCatIndex = Math.floor(Math.random() * 10) + 1;
+// //       const dancingCatsGIF = `dancing-cats${i}.gif`;
+// //       images[i].src = `dancing-cats${i}.gif`;
+// //     //   chrome.extension.getURL(dancingCatsGIF);
+
+// //     }, 3000)
+
+
+// const images = document.getElementsByTagName('img');
+//     // Loop through the images and replace the src with a random dancing cats GIF
+// for (let i = 0; i < images.length; i++) {
+//       const randomCatIndex = Math.floor(Math.random() * 10) + 1;
+//       const dancingCatsGIF = `dancing-cats${i}.gif`;
+//       images[i].src = `dancing-cats1.gif`;
+//     // chrome.extension.getURL(dancingCatsGI
 */
